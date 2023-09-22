eb461cd1c87941396577a1e28dddade29688167d
ddarshit <darshitdayala007@gmail.com>
2023-09-22 23:06:11 +0530

    project

--- src/Useradd.jsx
+++ src/Useradd.jsx
@@ -10,31 +10,33 @@ import {
 import { useNavigate } from "react-router-dom";
 
 function Useradd() {
-  const [id , setId] = useState("")
-  const [name , setName] = useState("")
-  const [email , setEmail] = useState("")
-  const [password , setPassword] = useState("")
-  const navigate = useNavigate()
+  const [id, setId] = useState(1);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-
-  const handlesubmit = (event) => {
-    event.preventDefault()
-    let data = {name , email ,password}
+  function handlesubmit() {
+    // event.preventDefault();
+    // let temp = localStorage.getItem("id");
+    let data = { name, email, password };
     // console.log(id ,name,email,password)
-    fetch("http://localhost:5000/user" ,{
-       method: "post",
-       headers: {
-        "Content-Type": "application/json"
-       },
-       body : JSON.stringify(data)
-    }).then((result) =>{
-      console.log(result)
-      result.json().then((resp)=>{
-        console.log(resp)
-        navigate("/userdata")
-      })
-    })
-  } 
+    fetch("http://localhost:5000/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((result) => {
+      console.log(result);
+      result.json().then((resp) => {
+        console.log(resp);
+        localStorage.setItem("id", id + 1);
+        navigate("/userdata");
+      });
+    });
+  }
+
   return (
     <>
       <h1>ADD USER</h1>
@@ -58,49 +60,54 @@ function Useradd() {
                 Log in
               </h3>
               {/* <form onSubmit={handlesubmit}> */}
-                <MDBInput
-                  wrapperClass="mb-4 mx-5 w-100"
-                  label="Id"
-                  disabled
-                  value={id}
-                  onChange={(e) => setId(e.target.value)}
-                  id="formControlLg1"
-                  type="text"
-                  size="lg"
-                />
-                <MDBInput
-                  wrapperClass="mb-4 mx-5 w-100"
-                  label="Name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  id="formControlLg2"
-                  type="text"
-                  size="lg"
-                />
-                <MDBInput
-                  wrapperClass="mb-4 mx-5 w-100"
-                  label="Email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  id="formControlLg3"
-                  type="email"
-                  size="lg"
-                />
-                <MDBInput
-                  wrapperClass="mb-4 mx-5 w-100"
-                  label="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  id="formControlLg4"
-                  type="password"
-                  size="lg"
-                />
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                label="Id"
+                disabled
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+                id="formControlLg"
+                type="text"
+                size="lg"
+              />
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                label="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                id="formControlLg"
+                type="text"
+                size="lg"
+              />
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                label="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                id="formControlLg"
+                type="email"
+                size="lg"
+              />
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                label="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                id="formControlLg"
+                type="password"
+                size="lg"
+              />
 
-                <MDBBtn className="mb-4 px-5 mx-5 w-100" onClick={handlesubmit} color="info" size="lg">
-                  Add User
-                </MDBBtn>
-                {/* <p className="small mb-5 pb-lg-3 ms-5"><a class="text-muted" href="#!">Forgot password?</a></p> */}
-                {/* <p className='ms-5'>Don't have an account? <a href="#!" class="link-info">Register here</a></p> */}
+              <MDBBtn
+                className="mb-4 px-5 mx-5 w-100"
+                onClick={handlesubmit}
+                color="info"
+                size="lg"
+              >
+                Add User
+              </MDBBtn>
+              {/* <p className="small mb-5 pb-lg-3 ms-5"><a class="text-muted" href="#!">Forgot password?</a></p> */}
+              {/* <p className='ms-5'>Don't have an account? <a href="#!" class="link-info">Register here</a></p> */}
               {/* </form> */}
             </div>
           </MDBCol>
