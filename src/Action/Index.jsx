fa6230e3f681a4f96149cc7d638e00fdbe9ab37a
ddarshit <darshitdayala007@gmail.com>
2023-10-08 17:20:49 +0530

    project

---
+++ src/Action/Index.jsx
@@ -0,0 +1,25 @@
+export const IncNumber = () => {
+    return{
+        type : "INCREMENT"
+    }
+}
+export const DecNumber = () => {
+    return{
+        type : "DECREMENT"
+    }
+}
+export const TexT = () => {
+    return{
+        type : "TEXT"
+    }
+}
+export const Change_bg = () => {
+    return{
+        type : "CHANGE_BG"
+    }
+}
+export const Reset_bg = () => {
+    return{
+        type : "RESET_BG"
+    }
+}
